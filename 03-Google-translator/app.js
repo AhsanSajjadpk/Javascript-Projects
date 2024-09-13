a4db34363a7aa829ff12e3915e481fadb0b2cf77
Ahsan Sajjad <princeahsan0608@gmail.com>
2024-09-14 01:55:29 +0500

    Frontend Ready..

---
+++ 03-Google-translator/app.js
@@ -0,0 +1,44 @@
+// All Dom Elements
+
+const sourcelanguage = document.getElementById('source-language')
+const targetlanguage = document.getElementById('target-language')
+const swapButton = document.getElementById('swap')
+const copyToClipboard = document.getElementById('copy-to-clipboard')
+const sourceLanguageText = document.getElementById('source-language-text')
+const targetLanguageText = document.getElementById('target-language-text')
+
+
+
+// All Variables
+
+
+// All functions
+
+
+// All Event Listener
+
+// Event Listener to track source language
+sourcelanguage.addEventListener('change',);
+
+
+// Event Listener to translate source language into target language
+targetlanguage.addEventListener('change',);
+
+
+// Event Listener to translate source language into target language afetr 3 second
+sourceLanguageText.addEventListener('input',);
+
+
+
+// Event Listener to swap text between source and target language
+swapButton.addEventListener('click',);
+
+
+
+// Event Listener to copy target language
+
+copyToClipboard.addEventListener('click',);
+
+
+
+// init
